2b8e5bc27851c34115f78b87f9bb9a35f95c6c5d
GH <982369788@qq.com>
2019-09-17 11:58:32 +0800

    bulid: 使用entry定义多个入口文件

--- lesson02/webpack.config.js
+++ lesson02/webpack.config.js
@@ -1,7 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index2.js',
 
     plugins: [new HtmlWebpackPlugin()]
 };
---
+++ lesson03/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports={
+    entry:{
+        index1:'./src/index1.js',
+        index2:'./src/index2.js',
+        index3:'./src/index3.js'
+
+    },
+    mode:'development'
+}
